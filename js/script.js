e5fa1cf8dd0ea7270b3890c0d9d4e782aa661bc5
Ogarkova Anastasiia <nastyab18@mail.ru>
2021-04-06 12:13:02 +0500

    code correction and added feedback form

--- js/script.js
+++ js/script.js
@@ -1,53 +1,30 @@
 $(function () {
   const $body = $('body');
   const $buttonFooter = $('.footer__btn');
-  const $buttonHeader = $('.header__btn');
   const $formAlert = $('<p class="alert alert_success">Ваша заявка отрпавлена успешно</p>');
-  const $formBody = $('.callback-form__body');
-  const $menu = $('.header__menu-icon');
+  const $formBody = $('.form__body');
+  const $menuIcon = $('.header__menu-icon');
+  const $nav = $('.nav');
   const $navItem = $('.nav__item');
-  const $overlay = $('<div class="overlay"></div>');
+  const $modalOverlay = $('.modal-overlay');
 
   function disableScroll() {
     $body.addClass('stop-scrolling');
-      $body.bind('touchmove', function (e) {
-        e.preventDefault();
-      });
+    $body.bind('touchmove', function (event) {
+      event.preventDefault();
+    });
   }
 
   function enableScroll() {
     $body.removeClass('stop-scrolling');
-      $body.unbind('touchmove');
+    $body.unbind('touchmove');
   }
 
-  $menu.on('click', function () {
-    $(this).toggleClass('active');
-    $('.nav').toggleClass('open-menu');
-    if ($menu.hasClass('active')) {
-      disableScroll();
-    } else {
-      enableScroll();
-    }
-  });
-
-  $navItem.on('click', function () {
-    if ($(document).outerWidth() <= 1024) {
-      $menu.toggleClass('active');
-      $('.nav').toggleClass('open-menu');
-      enableScroll();
-    }
-  });
-
-  function changeButtonHeader() {
-    if ($(document).outerWidth() <= 600) {
-      $buttonHeader.html('<i class="fa fa-phone" aria-hidden="true"></i>');
-    } else {
-      $buttonHeader.text('Заказать звонок');
-    }
+  function closeMenu() {
+    $menuIcon.removeClass('active');
+    $nav.removeClass('open-menu');
+    enableScroll();
   }
-
-  changeButtonHeader();
-  $(window).resize(changeButtonHeader);
 
   function changeButtonFooter() {
     if ($(document).outerWidth() <= 800) {
@@ -57,82 +34,153 @@ $(function () {
     }
   }
 
-  changeButtonFooter();
-  $(window).resize(changeButtonFooter);
+  function showModal(modal) {
+    $(modal).fadeIn(600);
+    disableScroll();
+  }
 
-  $('[rel=show-popup]').click(function () {
-    $('.popup_callback').fadeIn(700);
-    $body.append($overlay);
-    $overlay.show();
-    return false;
-  });
-
-  $('.popup__close').click(function () {
-    $(this).parent().fadeOut(500);
-    $overlay.remove();
-    return false;
-  });
-
-  $('#callback-phone').mask('+7 (999) 999-9999');
-
-  $('.callback-form').submit(function () {
-    const $form = $(this);
-
-    $form.find('.form-error').remove();
-
-    if ($form.find('input[name=phone]').val() === '') {
-      $form.find('label[for=callback-phone]')
-        .prepend('<div class="callback-form__error">Введите телефон</div>');
-      return false;
-    }
-
+  function sendForm(form) {
     $.post(
-      $form.attr('action'),
-      $form.serialize(),
+      $(form).attr('action'),
+      $(form).serialize(),
       function () {
         $formBody.hide();
-        $form.append($formAlert);
+        $(form).append($formAlert);
         setTimeout(() => {
-          $('.popup').fadeOut(500);
-          $overlay.remove();
+          $modalOverlay.fadeOut(400);
           $formAlert.remove();
           $formBody.show();
-          $('.callback-form__input').val('');
+          $(form).trigger('reset');
+          enableScroll();
         }, 2000);
       }
     );
+  }
 
-    return false;
-  });
-});
-
-const swiper = new Swiper('.portfolio__slider', {
-  slidesPerView: 1,
-  loop: true,
-  breakpoints: {
-    600: {
-      slidesPerView: 2,
-      spaceBetween: 35,
-    },
-    1025: {
-      slidesPerView: 3,
-      spaceBetween: 30,
+  $menuIcon.on('click', function () {
+    $(this).toggleClass('active');
+    $nav.toggleClass('open-menu');
+    if ($menuIcon.hasClass('active')) {
+      disableScroll();
+    } else {
+      enableScroll();
     }
-  },
-  slideClass: 'portfolio__item',
-  wrapperClass: 'portfolio__list',
-  pagination: {
-    el: '.portfolio__pagination',
-    type: 'bullets',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.portfolio__button_next',
-    prevEl: '.portfolio__button_prev',
-  },
-  a11y: {
-    prevSlideMessage: 'Предыдущий слайд',
-    nextSlideMessage: 'Следующий слайд',
-    paginationBulletMessage: 'Слайд №{{index}}',
-  },
+  });
+
+  $(window).resize(function (event) {
+    if (event.target.innerWidth > 1024 && $menuIcon.hasClass('active')) {
+      closeMenu();
+    }
+  });
+
+  $navItem.on('click', function () {
+    closeMenu();
+  });
+
+  $body.on('click', function (event) {
+    if ($nav.hasClass('open-menu')) {
+      const $menu = event.target.closest('.open-menu');
+      const $icon = event.target.closest('.header__menu-icon');
+
+      if ($menu || $icon) return;
+
+      closeMenu();
+    }
+  });
+
+  changeButtonFooter();
+  $(window).resize(changeButtonFooter);
+
+  $('.callback').on('click', function () {
+    showModal('.modal-callback');
+  });
+
+  $('.feedback').on('click', function () {
+    showModal('.modal-feedback');
+  });
+
+  $('.modal__close').on('click', function () {
+    $(this).parents('.modal-overlay').fadeOut(400);
+    $('.form').trigger('reset');
+    enableScroll();
+  });
+
+  $modalOverlay.on('click', function (event) {
+    const $modal = event.target.closest('.modal');
+
+    if ($modal) return;
+
+    $modalOverlay.fadeOut(400);
+    $('.form').trigger('reset');
+    enableScroll();
+  });
+
+  $('#callback-phone, #feedback-phone').mask('+7 (999) 999-9999');
+
+  $('.callback-form').validate({
+    errorElement: 'span',
+    focusInvalid: false,
+    rules: {
+      phone: 'required',
+    },
+    messages: {
+      phone: 'Введите телефон',
+    },
+    submitHandler(form) {
+      sendForm(form);
+    }
+  });
+
+  $('.feedback-form').validate({
+    errorElement: 'span',
+    focusInvalid: false,
+    rules: {
+      phone: 'required',
+      email: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      phone: 'Введите телефон',
+      email: {
+        required: "Введите E-mail",
+        email: "E-mail должен быть в формате name@domain.com"
+      }
+    },
+    submitHandler(form) {
+      sendForm(form);
+    }
+  });
+
+  new Swiper('.portfolio__slider', {
+    slidesPerView: 1,
+    loop: true,
+    breakpoints: {
+      600: {
+        slidesPerView: 2,
+        spaceBetween: 35,
+      },
+      1025: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      }
+    },
+    slideClass: 'portfolio__item',
+    wrapperClass: 'portfolio__list',
+    pagination: {
+      el: '.portfolio__pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.portfolio__button_next',
+      prevEl: '.portfolio__button_prev',
+    },
+    a11y: {
+      prevSlideMessage: 'Предыдущий слайд',
+      nextSlideMessage: 'Следующий слайд',
+      paginationBulletMessage: 'Слайд №{{index}}',
+    },
+  });
 });
